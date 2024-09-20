800a5decdfea4252194632645e56c73723f983f9
Metin Furkan Yaman <96432415+MFurkanYaman@users.noreply.github.com>
2024-09-20 16:04:22 +0300

    Update kutuphanesiz .py

--- K-Means/kutuphanesiz .py
+++ K-Means/kutuphanesiz .py
@@ -5,10 +5,18 @@ import math
 
 #Distance çalışıyor.
 #Centorid kümeleme hatalı düzeltildi.
+#Yeni centroidler hesaplanıldı.
 #Küme kontrol genel döngü eklenecek.
 #Best Score bulunacak.
 #Kod modüler hale dönüştürülecek ve kütüphaneli koda entegre edilicek.
 
+"""
+clusters:  Hangi noktanın hangi kümede bulunduğu bilgisi vardır.
+distance_dict:   Öklid sonucu uzaklık değerleri burada.
+new_centroids: yapılan kümeleme sonucunda yeni centroids seçimi gerçekleştirildi.
+
+
+"""
 
 
 
@@ -20,12 +28,12 @@ centroids_num=2
 
 
 value=[]
-old_dict={}
+distance_dict={}
 
-
+total=0
 find_min=[]
 sayac=0
-new_dict={}
+clusters={}
 
 #Bu kısım doğru
 # while True:
@@ -34,37 +42,50 @@ for i in range(centroids_num):
     for j in range(row_num):
         distance = np.sqrt(np.sum((scaled_data[j] - centroids[i])**2))
         value.append(distance)
-    old_dict[i]=value
+    distance_dict[i]=value
     value=[] 
 
-print(old_dict)
+# print(distance_dict)
 results = []
 index=0
 
 #Hatalı Düzeltildi.
+
+
+print(distance_dict)
 for i in range(row_num):
     min_value=float("inf")
     for j in range(centroids_num):
         
-        if old_dict[j][i]<min_value:
+        if distance_dict[j][i]<min_value:
 
-            min_value=old_dict[j][i]
+            min_value=distance_dict[j][i]
             min_key=j
 
-        if min_key not in new_dict:
-            new_dict[min_key]=[]
+        if min_key not in clusters:
+            clusters[min_key]=[]
 
-    new_dict[min_key].append(i)
+    clusters[min_key].append(i)
     results.append(f"Index {i}: key {min_key} değeri ({min_value}) daha küçük.")
 
 
-for result in results:
-    print(result)
+
+# yeni centroidi yap.
+
+new_centroids=[]
+for i in range(centroids_num):
+
+    for inside_value in clusters[i]:
+        total+=scaled_data[inside_value]
+    # print(len(clusters[i]))
+    total//=len(clusters[i])
+    new_centroids.append(total)
+    total=0
+
+#yeni centroidsler:
+centroids = np.array(new_centroids)
 
 
-print(new_dict)
 
-
-    
 
 
