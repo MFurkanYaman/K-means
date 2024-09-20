79808c27bf8b0494d8a3b8ab444412899d53c7a4
Metin Furkan Yaman <96432415+MFurkanYaman@users.noreply.github.com>
2024-09-20 17:16:26 +0300

    Update kutuphanesiz .py

--- K-Means/kutuphanesiz .py
+++ K-Means/kutuphanesiz .py
@@ -10,11 +10,11 @@ import math
 #Best Score bulunacak.
 #Kod modüler hale dönüştürülecek ve kütüphaneli koda entegre edilicek.
 
+
 """
 clusters:  Hangi noktanın hangi kümede bulunduğu bilgisi vardır.
 distance_dict:   Öklid sonucu uzaklık değerleri burada.
 new_centroids: yapılan kümeleme sonucunda yeni centroids seçimi gerçekleştirildi.
-
 
 """
 
@@ -29,63 +29,81 @@ centroids_num=2
 
 value=[]
 distance_dict={}
-
-total=0
+total = np.zeros(scaled_data.shape[1], dtype=np.float64)
 find_min=[]
 sayac=0
 clusters={}
-
+old_clusters={}
+kontrol =True
 #Bu kısım doğru
 # while True:
-    
-for i in range(centroids_num):          
-    for j in range(row_num):
-        distance = np.sqrt(np.sum((scaled_data[j] - centroids[i])**2))
-        value.append(distance)
-    distance_dict[i]=value
-    value=[] 
-
-# print(distance_dict)
-results = []
-index=0
-
-#Hatalı Düzeltildi.
-
-
-print(distance_dict)
-for i in range(row_num):
-    min_value=float("inf")
-    for j in range(centroids_num):
-        
-        if distance_dict[j][i]<min_value:
-
-            min_value=distance_dict[j][i]
-            min_key=j
-
-        if min_key not in clusters:
-            clusters[min_key]=[]
-
-    clusters[min_key].append(i)
-    results.append(f"Index {i}: key {min_key} değeri ({min_value}) daha küçük.")
-
-
-
-# yeni centroidi yap.
-
-new_centroids=[]
+empty_list=[]
 for i in range(centroids_num):
-
-    for inside_value in clusters[i]:
-        total+=scaled_data[inside_value]
-    # print(len(clusters[i]))
-    total//=len(clusters[i])
-    new_centroids.append(total)
-    total=0
-
-#yeni centroidsler:
-centroids = np.array(new_centroids)
+   old_clusters[i]=empty_list
 
 
 
+
+while kontrol==True:
+
+    for i in range(centroids_num):          
+        for j in range(row_num):
+            distance = np.sqrt(np.sum((scaled_data[j] - centroids[i])**2))
+            value.append(distance)
+        distance_dict[i]=value
+        value=[] 
+
+    # print(distance_dict)
+    results = []
+    index=0
+
+    #Hatalı Düzeltildi.
+
+    #Gruplandırma 
+    # print(distance_dict)
+    for i in range(row_num):
+        min_value=float("inf")
+        for j in range(centroids_num):
+            
+            if distance_dict[j][i]<min_value:
+
+                min_value=distance_dict[j][i]
+                min_key=j
+
+            if min_key not in clusters:
+                clusters[min_key]=[]
+
+        clusters[min_key].append(i)
+        results.append(f"Index {i}: key {min_key} değeri ({min_value}) daha küçük.")
+
+
+
+    # yeni centroidi hesapla.
+
+    new_centroids=[]
+    for i in range(centroids_num):
+
+        for inside_value in clusters[i]:
+            total+=scaled_data[inside_value]
+        # print(len(clusters[i]))
+        total/=len(clusters[i])
+        new_centroids.append(total)
+        total = np.zeros(scaled_data.shape[1], dtype=np.float64)
+    
+    centroids=new_centroids
+    sayac+=1
+
+
+#Hata olmalı ?
+    for key in clusters:
+        if(sorted(clusters[key])==old_clusters[key]):
+            kontrol=False
+            
+    old_clusters=clusters
+    print(old_clusters)
+    clusters.clear()
+    print(clusters) 
+    
+print(sayac)
 
 
